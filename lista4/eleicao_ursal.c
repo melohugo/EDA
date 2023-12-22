b281a43873cee5ce6f598a7ef4f38b11919e4d07
melohugo <hugoq2004@gmail.com>
2023-12-22 19:45:34 -0300

    Subindo eleição ursal 100p

--- lista4/eleicao_ursal.c
+++ lista4/eleicao_ursal.c
@@ -9,88 +9,76 @@
 #define swap(c1, c2) {int c = c1; c1 = c2; c2 = c;}
 
 typedef struct dado{
-	int numero;
-	int votos;
+	long long numero;
+	long long votos;
 } dado;
 
 void merge(dado *v, int l, int m, int r)
 {
 	int i, j, k;
 	dado *aux;
-
 	aux = malloc(sizeof(dado)*(r + 1 - l));
 	k = 0;
 	for (i = l, j = m + 1; i <= m && j <= r; )
 		aux[k++] = v[i].votos <= v[j].votos ? v[i++] : v[j++];
-
 	while (i <= m)
 		aux[k++] = v[i++];
-
 	while (j <= r)
 		aux[k++] = v[j++];
-
 	for (int x = 0; l <= r; l++, x++)
 		v[l] = aux[x];
-
 	free(aux);
 }
-
 void merge_sort(dado *v, int l, int r)
 {
 	int m;
-
 	if (l >= r)
 		return;
-
 	m = l + (r - l)/2;
-
 	merge_sort(v, l, m);
 	merge_sort(v, m + 1, r);
 	merge(v, l, m, r);
 }
-
 int main(void)
 {
-	int S, F, E;
-	int validos, invalidos;
-	int c;
+	long long S, F, E;
+	long long validos, invalidos;
+	long long votosp;
+	long long c;
 	dado *politicos;
-
 	politicos = calloc(1000000,sizeof(dado));
-	validos = invalidos = 0;
+	validos = invalidos = votosp = 0;
 	
-	scanf("%d %d %d", &S, &F, &E);
-	while(scanf("%d", &c) != EOF) {
-		if (c >= 0) {
+	scanf("%lld %lld %lld", &S, &F, &E);
+	while(scanf("%lld", &c) != EOF) {
+		if (c >= 10) {
 			politicos[c].numero = c;
 			politicos[c].votos += 1;
 			validos++;
 		}
 		else
 			invalidos++;
+		if (c < 100 && c > 1)
+			votosp++;
 	}
-
 	
-	merge_sort(politicos, 0, 99);
+	merge_sort(politicos, 10, 99);
 	merge_sort(politicos, 100, 999);
 	merge_sort(politicos, 1000, 9999);
 	merge_sort(politicos, 10000, 99999);
 	
-	printf("%d %d\n", validos, invalidos);
-	if (politicos[99].votos == politicos[98].votos)
-		printf("Segundo turno\n");
+	printf("%lld %lld\n", validos, invalidos);
+	if (((float)politicos[99].votos/(float)votosp) * 100 >= 51)
+		printf("%lld\n", politicos[99].numero);
 	else
-		printf("%d\n", politicos[99].numero);
+		printf("Segundo turno\n");
 	
-	for (int i = 0; i < S; i++)
-		printf("%d%c", politicos[999 - i].numero, i + 1 == S ? '\n' : ' ');
-
-	for (int i = 0; i < F; i++)
-		printf("%d%c", politicos[9999 - i].numero, i + 1 == F ? '\n' : ' ');
+	for (long long i = 0; i < S; i++)
+		printf("%lld%c", politicos[999 - i].numero, i + 1 == S ? '\n' : ' ');
+	for (long long i = 0; i < F; i++)
+		printf("%lld%c", politicos[9999 - i].numero, i + 1 == F ? '\n' : ' ');
 	
-	for (int i = 0; i < E; i++)
-		printf("%d%c", politicos[99999 - i].numero, i + 1 == E ? '\n' : ' ');
-
-
+	for (long long i = 0; i < E; i++)
+		printf("%lld%c", politicos[99999 - i].numero, i + 1 == E ? '\n' : ' ');
 	return 0;
 }
